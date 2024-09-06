3d3b33891f5f4bf86c37d4aaa4b451893f200bf8
Leezan <leezandawzdi@gmail.com>
2024-09-06 11:31:44 +0545

    modified stack folder such that a single stack class of stack.cpp is applied to all operations in stack

--- Stack/PostfixOperation.cpp
+++ Stack/PostfixOperation.cpp
@@ -1,78 +1,11 @@
 #include <iostream>
 #include <string>
+#include "Stack.cpp"
 
-#define STACK_SIZE 5
 #define POSTFIX_EXP "34 4 + 4 5 - *"
 
 using namespace std;
 
-class Stack
-{
-private:
-    int top, stack_array[STACK_SIZE];
-
-public:
-    Stack()
-    {
-        top = -1;
-        for (size_t i = 0; i < STACK_SIZE; i++)
-        {
-            stack_array[i] = 0;
-        }
-    }
-
-    bool isFull()
-    {
-        return (top == STACK_SIZE - 1) ? true : false;
-    }
-
-    bool isEmpty()
-    {
-        return (top == -1) ? true : false;
-    }
-
-    int push(int val)
-    {
-        if (isFull())
-        {
-            cout << "Stack Overflown";
-            return -1;
-        }
-        else
-        {
-            top++;
-            stack_array[top] = val;
-            return top;
-        }
-    }
-
-    int pop()
-    {
-        if (isEmpty())
-        {
-            cout << "Stack Underflow" << endl;
-            return -1;
-        }
-        else
-        {
-            int popped_value = stack_array[top];
-            stack_array[top] = 0;
-            top = top - 1;
-            return popped_value;
-        }
-    }
-
-    void display()
-    {
-        cout << "All values in the stack are" << endl;
-        for (int i = STACK_SIZE - 1; i >= 0; i--)
-        {
-            cout << "|" << "\t" << stack_array[i] << "\t" << "|" << endl;
-        }
-        cout << "Top value is: " << top
-             << endl;
-    }
-};
 
 int postfixOperation(int first_operand, int second_operand, string operators)
 {
@@ -141,6 +74,7 @@ int main()
     }
     
     stack1.display();
-    cout<<"The final result of postfix expression is: "<<stack1.pop()<<endl;
+    int final_result = stack1.pop();
+    cout<<"The final result of postfix expression is: "<<final_result<<endl;
     return 0;
 }
