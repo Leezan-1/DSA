7343d7d1576b31dcf133c26ae3e2b9f07b2b9dd4
Leezan <leezandawzdi@gmail.com>
2024-09-09 17:51:14 +0545

    Fibonacci Sequence complete

---
+++ Recursion/FibonacciSequence.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned int fibonacci(int n)
+{
+
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    unsigned int length = 30;
+
+    cout << "Enter the length of fibonacci series: " << endl;
+    // cin>>length;
+
+    cout << "The fibonacci sequences are: ";
+    for (size_t i = length; i >= 0; i--)
+    {
+        if (i == 0)
+            cout << fibonacci(i)<<endl;
+        else
+            cout << fibonacci(i) << ", ";
+    }
+
+    return 0;
+}
