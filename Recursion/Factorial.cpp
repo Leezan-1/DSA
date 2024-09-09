933170946682fe69d648beb4c44b6e33ae5ea304
Leezan <leezandawzdi@gmail.com>
2024-09-09 15:30:00 +0545

    made user input and final output display

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -2,12 +2,21 @@
 
 using namespace std;
 
-int factorial()
+int factorial(int n)
 {
-    return 0;
+    return 0; 
 }
 
 int main()
 {
+    size_t number;
+
+    // User Input
+    cout<<"Enter the number that you want to find factorial of: ";
+    cin>>number;
+
+
+    // Final Output
+    cout<<number<<"! is: "<<factorial(number)<<endl;
     return 0;
 }
