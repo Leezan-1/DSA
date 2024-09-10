d969f79dabe09e8e85c03dd6653890da0abd1809
Leezan <leezandawzdi@gmail.com>
2024-09-10 10:46:04 +0545

    finished string reversal and added checking for palindrome

--- Recursion/StringReversal.cpp
+++ Recursion/StringReversal.cpp
@@ -4,25 +4,31 @@ using namespace std;
 
 string reverse_string(string usr_string, int start, int end)
 {
-    cout<<usr_string<<start<<end<<endl;
     if (start >= end)
         return usr_string;
-    
-    cout<<usr_string[0];
-    return "ABC";
 
+    swap(usr_string[start], usr_string[end]);
+    return reverse_string(usr_string, start + 1, end - 1);
 }
 
 int main()
 {
-    string user_string = "ABC", reversed_string;
+    string user_string = "candna", reversed_string;
 
-    cout << "Enter the string to reverse: "<<user_string<<endl;;
+    cout << "Enter the string to reverse: " << user_string << endl;
     // cin >> user_string;
 
-    int start =0, end =  user_string.length()-1;
-
+    // logic for string reversal
+    int start = 0, end = user_string.length() - 1;
     reversed_string = reverse_string(user_string, start, end);
-    cout << "The reverse of your string is: " << reversed_string<<endl;
+
+
+    cout << "The reverse of your string is: " << reversed_string << endl;
+
+    if (reversed_string == user_string)
+        cout<<"It is a palindrome!"<<endl;
+    else
+        cout<<"It is not a palindrome!"<<endl;
+
     return 0;
 }
