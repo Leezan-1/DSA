bbb8d328d1d6bfd793aee9233e9f3202779778c5
Leezan <leezandawzdi@gmail.com>
2024-09-18 22:22:29 +0545

    made single linked list in C, using structures. Also made 2 static nodes of linkedlist, also made a function to addnode at the end of linked list, made a count function to count the number of nodes and made a display function that displays linked list

---
+++ Linked List/SinglyLinkedList.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// linked list using structure
+typedef struct Node // This create a variable acting node of linked list
+{
+    int data;
+    struct Node *link;
+} Node;
+
+const int NODE_SIZE = sizeof(Node);
+
+int countNodes(Node *head) // This function counts the number of nodes present in linked list
+{
+    int count = 0;
+    if (head == NULL)
+        return count;
+
+    while (head != NULL)
+    {
+        count++;
+        head = head->link;
+    }
+    return count;
+}
+
+void displayLinkedlist(Node *head) // This function prints the linked list;
+{
+    if (head == NULL)
+    {
+        printf("linked list is empty!\n");
+        return;
+    }
+    printf("The Linked list is: |%p| ", head);
+    while (head != NULL)
+    {
+        printf("----> |%d|%p| ", head->data, head->link);
+        head = head->link;
+    }
+}
+
+void insertNodeAtEnd(Node *head, int value) // This function inserts node with a value at the end of linked list
+{
+    Node *temp = malloc(NODE_SIZE);
+    temp->data = value;
+    temp->link = NULL;
+
+    while (head->link != NULL)
+        head = head->link;
+
+    head->link = temp;
+}
+
+int main()
+{
+    Node *head = NULL;
+
+    // the first node of the linked list
+    head = malloc(NODE_SIZE);
+    head->data = 5;
+    head->link = NULL;
+
+    // The second node of linked list
+    {
+        Node *current = malloc(NODE_SIZE);
+        current->data = 10;
+        current->link = NULL;
+        head->link = current;
+        /* 
+        The data located at malloc doesnot gets lost until it is freed 
+         even if the 'current' pointer variable gets delete at the end of the scope
+        */
+    }
+
+    // This is the third node of linked list
+    insertNodeAtEnd(head, 25);
+
+    // This is the fourth node of linked list
+    insertNodeAtEnd(head, 30);
+
+    printf("The number of nodes in linked list is %d \n", countNodes(head));
+    displayLinkedlist(head);
+
+    return 0;
+}
