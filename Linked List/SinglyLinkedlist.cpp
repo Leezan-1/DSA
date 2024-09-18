0bd6e0a20ef1fb365d1e2f18b52bcd5a20a5a68d
Leezan <leezandawzdi@gmail.com>
2024-09-18 22:23:44 +0545

    made single linked list with 3 static nodes in C++ using classes but havent added functions yet

---
+++ Linked List/SinglyLinkedlist.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+
+
+// linkedlist using class
+class Node
+{
+public:
+    int data;
+    Node *link;
+
+    Node()
+    {
+        data = INT_MIN;
+        link = nullptr;
+    }
+
+    Node(int value)
+    {
+        data = value;
+        link = nullptr;
+    }
+    Node(int value, Node *prev_node_addr, Node *node_addr)
+    {
+        data = value;
+        link = nullptr;
+        prev_node_addr->link = node_addr;
+    }
+};
+
+// class SinglyLinkedList
+// {
+// private:
+//     Node *head;
+//     Node *current;
+
+// public:
+//     SinglyLinkedList(int num_of_nodes)
+//     {
+//         head = (Node *)malloc(sizeof(Node));
+//         Node nodes[num_of_nodes];
+//     }
+
+//     Node *createNode(int num_of_nodes)
+//     {
+//         for (size_t i = 0; i < num_of_nodes; i++)
+//         {
+//             cout << "Enter the data in " << i << "th node";
+//             cin >> current->data;
+
+//         }
+//     }
+// };
+
+int main()
+{
+    Node *head = nullptr;
+
+    // First node of linked list
+    Node *node = new Node[3];
+    head = node;
+
+    node[0] = Node(45);
+    node[1] = Node(55, &node[0], &node[1]);
+    node[2] = Node(65, &node[1], &node[2]);
+
+    cout << head << "|------>|"
+         << node[0].data << " | " << node[0].link << " |------>| "
+         << node[1].data << " | " << node[1].link << " |------>| "
+         << node[2].data << " | " << node[2].link << " |" << endl;
+    
+    cout << head << "|------>|"
+         << head->data << " | " << head->link << " |------>| "
+         << head->link->data << " | " << head->link->link << " |------>| "
+         << head->link->link->data << " | " << head->link->link->link << " |" << endl;
+
+    delete[] node;
+}
