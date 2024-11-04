a21c010b6dc96e1922a46fea59edc9c04c8cc190
Leezan <leezandawzdi@gmail.com>
2024-11-04 13:59:20 +0545

    created functions to count nodes, display nodes and insert node at front

--- Linked List/CircularDoublyLinkedList.h
+++ Linked List/CircularDoublyLinkedList.h
@@ -1,6 +1,4 @@
-#include <iostream>
 
-using namespace std;
 
 /* Circular linked list is the type of linked list in which last node points
     to the first node.
@@ -23,7 +21,7 @@ private:
     } Node;
 
     Node *tail;
-    int value[0];
+    int array[6];
 
     Node *createNode(int value)
     {
@@ -47,16 +45,45 @@ private:
 
     bool checkEmpty()
     {
-        if (tail = nullptr)
+        if (tail == nullptr)
+        {
+            cout << "Linked list empty!";
             return true;
+        }
         else
             return false;
+    }
+
+    int countNodes()
+    {
+        int count = 0;
+        Node *temp = tail->next_addr;
+        do
+        {
+            count++;
+            temp = temp->next_addr;
+        } while (temp != tail->next_addr);
+        return count;
+
+        /*
+            the loop is changed compared to singly list as there is no null at
+            last node;
+            The loop runs as temp_node being the first node (tail's next_addr).
+            do...while is used here because we check the tail's next_addr(i.e front node) to end the loop.
+            and we start loop as the front node which will take us out of loop before even starting (if we use whiile loop).
+
+            The time complecity to print is O(n) as each node is travesed.
+        */
     }
 
 public:
     CircularDoubly()
     {
-        tail = createNode(value[1]);
+        for (size_t i = 0; i < 6; i++)
+        {
+            array[i] = i * 10;
+        }
+        tail = createNode(array[1]);
         tail->next_addr = tail;
         tail->prev_addr = tail;
         cout << "Circular Doubly Linked List Created!" << endl;
@@ -68,12 +95,65 @@ public:
             So tail's next_addr also points to itself.
         */
     }
-    
+
+    void displayNodes()
+    {
+        if (checkEmpty())
+            return;
+
+        cout << endl
+             << "Number of Node(s): " << countNodes() << endl;
+
+        Node *temp_node = tail->next_addr;
+        do
+        {
+            printf("| %d |\t", temp_node->data);
+            temp_node = temp_node->next_addr;
+        } while (temp_node != tail->next_addr);
+        cout << endl;
+
+        /*
+             This function prints the linked list;
+            the loop is changed compared to singly list as there is no null at
+            last node;
+            The loop runs as temp_node being the first node (tail's next_addr).
+            do...while is used here because we check the tail's next_addr(i.e front node) to end the loop.
+            and we start loop as the front node which will take us out of loop before even starting (if we use whiile loop).
+
+            The time complecity to print is O(n) as each node is travesed.
+        */
+    }
+
     bool insertNodeAtFront()
     {
         if (checkEmpty())
             return false;
-        return true;
-    }
 
+        Node *temp = createNode(array[0]);
+        temp->prev_addr = tail;
+        temp->next_addr = tail->next_addr;
+
+        tail->next_addr->prev_addr = temp;
+        tail->next_addr = temp;
+
+        if (tail == tail->next_addr)
+            tail->prev_addr = temp;
+        return true; 
+        
+        /*
+            first a new node is created and its addr is pointed by temp
+            
+            the new node's prev_addr will point to the tail and next_addr
+            will point to existing first node.
+
+            then we update the first node's prev_addr to point to temp as it will be 2nd node.
+            then we update tail's next_addr i.e firstnode to be temp;
+
+            if there is only one node then
+                tail's prev node should be temp;
+
+
+            ten first
+        */
+    }
 };
